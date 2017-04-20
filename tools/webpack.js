29fc72a8671360de3afc0e402dc122bfa78dad66
游品尧 <497400448@qq.com>
2017-04-20 09:28:15 +0800

    no message

--- tools/webpack.js
+++ tools/webpack.js
@@ -74,9 +74,9 @@ default:
 function runCallback(err, stats) {
 
   if (err) {
-    console.error(err.stack || err);
+    console.error(chalk.red(err.stack || err));
     if (err.details) {
-      console.error(err.details);
+      console.error(chalk.red(err.details));
     }
     return false;
   }
@@ -84,11 +84,11 @@ function runCallback(err, stats) {
   const info = stats.toJson();
 
   if (stats.hasErrors()) {
-    console.error(info.errors);
+    console.error(chalk.red(info.errors));
   }
 
   if (stats.hasWarnings()) {
-    console.warn(info.warnings);
+    console.warn(chalk.yellow(info.warnings));
   }
 
   return true;
